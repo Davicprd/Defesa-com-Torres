08f24e2f5a2028a38c4c005abf0ec844fcd9428d
Reulisson Torres <reulissongabriel@gmail.com>
2023-12-04 18:39:12 -0300

    Add Test Case 2 at denfense_towers_solve.py

--- defense_towers_solver.py
+++ defense_towers_solver.py
@@ -33,15 +33,15 @@ def ReplaceCountT(String):
     - result: The map layout with towers replaced by numbers.
     """
 
-    count_t = 0
+    tower_row = 0
     result = ""
     number_t = 0
 
     for line in String:
         for char in line:
             if char == "T":
-                count_t += 1
-                result += str(count_t)
+                tower_row += 1
+                result += str(tower_row)
                 number_t += 1
             else:
                 result += char
@@ -61,21 +61,21 @@ def CheckAttackers(String):
     - None
     """
 
-    count_k = 0
-    count_j = 0
-    count_t = 0
+    row_index = 0
+    col_index = 0
+    tower_row = 0
     for line in String:
         for char in line:
             if char == "n":
                 Solver.add(
                     Or(
-                        FindHorizontalT(String, count_k, count_j, count_t),
-                        FindVerticalT(String, count_k, count_j),
+                        FindHorizontalT(String, row_index, col_index, tower_row),
+                        FindVerticalT(String, row_index, col_index),
                     )
                 )
-            count_j += 1
-        count_j = 0
-        count_k += 1
+            col_index += 1
+        col_index = 0
+        row_index += 1
 
 
 def CheckCrossFire(String):
@@ -89,22 +89,22 @@ def CheckCrossFire(String):
     - None
     """
     
-    count_k = 0
-    count_j = 0
-    count_t = 0
+    row_index = 0
+    col_index = 0
+    tower_row = 0
     for line in String:
         for char in line:
             for i in range(1, TotalT + 1):
                 if char == f"{i}":
                     Solver.add(
                         Not(
-                            FindHorizontalT(String, count_k, count_j, count_t, i)
+                            FindHorizontalT(String, row_index, col_index, tower_row, i)
                         )
                     )
-                    Solver.add(Not(FindVerticalT(String, count_k, count_j, i)))
-            count_j += 1
-        count_j = 0
-        count_k += 1
+                    Solver.add(Not(FindVerticalT(String, row_index, col_index, i)))
+            col_index += 1
+        col_index = 0
+        row_index += 1
 
 
 def FormValuedMap(String):
@@ -157,7 +157,7 @@ def FormValuedMap(String):
     return valuedMap
 
 
-def FindHorizontalT(String, count_k, count_j, count_t, index=0):
+def FindHorizontalT(String, row_index, col_index, tower_row, index=0):
     """
     Checks for horizontal obstacles (castle or crossfire) for a particular cannon.
 
@@ -175,17 +175,17 @@ def FindHorizontalT(String, count_k, count_j, count_t, index=0):
     count = 0
     castle = False
     crossFire = False
-    for char in String[count_k]:
+    for char in String[row_index]:
         for f in range(1, TotalT + 1):
             if char == f"{f}" and int(char) != index:
-                if count_j < count_t:
+                if col_index < tower_row:
                     i = count
-                    while i != count_j:
-                        if String[count_k][i] == "#":
+                    while i != col_index:
+                        if String[row_index][i] == "#":
                             castle = True
                         elif (
-                            String[count_k][i].isdecimal()
-                            and int(String[count_k][i]) != f
+                            String[row_index][i].isdecimal()
+                            and int(String[row_index][i]) != f
                             and index == 0
                         ):
                             crossFire = True
@@ -197,12 +197,12 @@ def FindHorizontalT(String, count_k, count_j, count_t, index=0):
                         crossFire = False
                         continue
                 else:
-                    for k in range(count, count_j):
-                        if String[count_k][k] == "#":
+                    for k in range(count, col_index):
+                        if String[row_index][k] == "#":
                             castle = True
                         elif (
-                            String[count_k][k].isdecimal()
-                            and int(String[count_k][k]) != f
+                            String[row_index][k].isdecimal()
+                            and int(String[row_index][k]) != f
                             and index == 0
                         ):
                             crossFire = True
@@ -213,12 +213,12 @@ def FindHorizontalT(String, count_k, count_j, count_t, index=0):
                         castle = False
                         continue
         count += 1
-        count_t += 1
-    count_t = 0
+        tower_row += 1
+    tower_row = 0
     return False
 
 
-def FindVerticalT(String, count_k, count_j, index=0):
+def FindVerticalT(String, row_index, col_index, index=0):
     """
     Checks for vertical obstacles (castle or crossfire) for a particular cannon.
 
@@ -237,14 +237,14 @@ def FindVerticalT(String, count_k, count_j, index=0):
     crossFire = False
     for i in range(0, r):
         for l in range(1, TotalT + 1):
-            if String[i][count_j] == f"{l}" and int(String[i][count_j]) != index:
-                if count_k < i:
-                    for k in range(count_k, i):
-                        if String[k][count_j] == "#":
+            if String[i][col_index] == f"{l}" and int(String[i][col_index]) != index:
+                if row_index < i:
+                    for k in range(row_index, i):
+                        if String[k][col_index] == "#":
                             castle = True
                         elif (
-                            (String[k][count_j].isdecimal())
-                            and int(String[k][count_j]) != l
+                            (String[k][col_index].isdecimal())
+                            and int(String[k][col_index]) != l
                             and index == 0
                         ):
                             crossFire = True
@@ -255,12 +255,12 @@ def FindVerticalT(String, count_k, count_j, index=0):
                         castle = False
                         continue
                 else:
-                    for k in range(i, count_k):
-                        if String[k][count_j] == "#":
+                    for k in range(i, row_index):
+                        if String[k][col_index] == "#":
                             castle = True
                         elif (
-                            (String[k][count_j].isdecimal())
-                            and int(String[k][count_j]) != l
+                            (String[k][col_index].isdecimal())
+                            and int(String[k][col_index]) != l
                             and index == 0
                         ):
                             crossFire = True
@@ -273,6 +273,7 @@ def FindVerticalT(String, count_k, count_j, index=0):
     return False
 
 
+# Test Case 1
 input_string = """9 8
 n.Tnnnnn
 nnnnnnTn
@@ -308,3 +309,37 @@ else:
     print("Unsatisfiable formula")
 
 print(FormValuedMap(map_data))
+
+# Test Case 2
+additional_input = """6 7
+..nT...
+Tnn..T.
+..T....
+n......
+T....Tn
+..n...."""
+
+additional_lines = additional_input.strip().split("\n")
+additional_dimensions = list(map(int, additional_lines[0].split()))
+additional_r, additional_s = additional_dimensions[0], additional_dimensions[1]
+additional_map_data = additional_lines[1:]
+
+TestDimensions(additional_map_data, additional_r, additional_s)
+TotalT, additional_map_data = ReplaceCountT(additional_map_data)
+
+additional_map_data = additional_map_data.split("\n")
+additional_map_data = additional_map_data[0:additional_r]
+
+Solver = Solver()
+VarHor = [Bool("t" + str(i) + "e") for i in range(1, TotalT + 1)]
+VarVer = [Bool("t" + str(i) + "c") for i in range(1, TotalT + 1)]
+
+CheckAttackers(additional_map_data)
+CheckCrossFire(additional_map_data)
+
+if Solver.check() == sat:
+    m = Solver.model()
+else:
+    print("Unsatisfiable formula")
+
+print(FormValuedMap(additional_map_data))
